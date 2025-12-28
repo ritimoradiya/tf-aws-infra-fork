f8952fd38b61f23c92b2d20f9d1d3ed862e6e9c3
Riti Moradiya <ritimoradiya1102@gmail.com>
2025-12-28 18:19:34 -0500

    updated main,outputs,variables

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,18 @@ output "private_subnet_ids" {
   description = "IDs of private subnets"
   value       = aws_subnet.private[*].id
 }
+
+output "application_security_group_id" {
+  description = "ID of application security group"
+  value       = aws_security_group.application.id
+}
+
+output "ec2_instance_id" {
+  description = "ID of EC2 instance"
+  value       = aws_instance.webapp.id
+}
+
+output "ec2_instance_public_ip" {
+  description = "Public IP of EC2 instance"
+  value       = aws_instance.webapp.public_ip
+}
--- variables.tf
+++ variables.tf
@@ -26,3 +26,20 @@ variable "aws_profile" {
   type        = string
   default     = "devv"
 }
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "app_port" {
+  description = "Application port"
+  type        = number
+  default     = 8080
+}
